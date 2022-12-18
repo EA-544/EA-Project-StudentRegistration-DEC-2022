8a9f8ec269416da303dfe6ccdbdd0e098bd22b5c
GraceTatiana <112901608+GraceTatiana@users.noreply.github.com>
2022-12-17 20:35:34 -0600

    first commit

--- project/src/main/java/CourseRegistration/project/ProjectApplication.java
+++ project/src/main/java/CourseRegistration/project/ProjectApplication.java
@@ -3,16 +3,15 @@ package CourseRegistration.project;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class ProjectApplication implements CommandLineRunner {
+@SpringBootApplication(scanBasePackages = {"CourseRegistration.project"})
+@EnableJpaRepositories(basePackages = {"CourseRegistration.project.Repository"})
+@EntityScan(basePackages = {"CourseRegistration.project"})
+public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 	}
 }
--- project/src/main/java/CourseRegistration/project/Repository/AcadamicBlockRepository.java
+++
@@ -1,9 +0,0 @@
-package CourseRegistration.project.Repository;
-
-import CourseRegistration.project.domain.AcademicBlock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AcadamicBlockRepository extends JpaRepository<AcademicBlock,Integer> {
-}
